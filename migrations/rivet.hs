57213513b775b875b85dc996eb7cdeae36fcec4e
Ian Davidson <ian.davidson@onepeloton.com>
2020-05-27 17:32:37 -0400

    Hardcode everything for the migrator

--- migrations/rivet.hs
+++ migrations/rivet.hs
@@ -11,21 +11,22 @@ import qualified Database.Rivet.Main               as Rivet
 import           System.Directory                  (doesFileExist)
 import           System.Environment
 
-
-main = do e <- doesFileExist ".env"
-          when e $ Configuration.Dotenv.loadFile False ".env"
-          args <- getArgs
-          let (env, mode) =
-               case args of
-                 [env', "up"] -> (env', Rivet.MigrateUp)
-                 [env', "down"] -> (env', Rivet.MigrateDown)
-                 [env', "status"] -> (env', Rivet.MigrateStatus)
-                 _ -> error "Usage: [executable] [devel|prod|...] [up|down|status]"
-          conf <- C.load [C.Required (env <> ".cfg")]
-          host <- C.require conf "postgresql-simple.host"
-          port <- C.require conf "postgresql-simple.port"
-          user <- C.require conf "postgresql-simple.user"
-          pass <- C.require conf "postgresql-simple.pass"
-          db <- C.require conf "postgresql-simple.db"
-          adaptor <- setup id (ConnectInfo host port user pass db)
-          Rivet.main adaptor mode migrations
+main :: IO ()
+main = do -- e <- doesFileExist ".env"
+          -- when e $ Configuration.Dotenv.loadFile False ".env"
+          -- args <- getArgs
+          -- let (env, mode) =
+          --      case args of
+          --        [env', "up"] -> (env', Rivet.MigrateUp)
+          --        [env', "down"] -> (env', Rivet.MigrateDown)
+          --        [env', "status"] -> (env', Rivet.MigrateStatus)
+          --        _ -> error "Usage: [executable] [devel|prod|...] [up|down|status]"
+          -- conf <- C.load [C.Required (env <> ".cfg")]
+          -- host <- C.require conf "postgresql-simple.host"
+          -- port <- C.require conf "postgresql-simple.port"
+          -- user <- C.require conf "postgresql-simple.user"
+          -- pass <- C.require conf "postgresql-simple.pass"
+          -- db <- C.require conf "postgresql-simple.db"
+  let mode = Rivet.MigrateUp
+  adaptor <- setup id (ConnectInfo "localhost" 5432 "" "" "howston")
+  Rivet.main adaptor mode migrations
