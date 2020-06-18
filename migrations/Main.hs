5b4eb979e581b4673bd9173c5e0ac039a2cc84c5
Ian Davidson <ian.davidson@onepeloton.com>
2020-06-18 17:02:43 -0400

    rough list of migrations from folder

--- migrations/Main.hs
+++ migrations/Main.hs
@@ -7,9 +7,30 @@ import Data.List
 filterDirs :: [FilePath] -> [FilePath]
 filterDirs = filter (\s -> not $ ".hs" `isSuffixOf` s)
 
-main = do
-  -- collect files
-  files <- filterDirs <$> listDirectory "./migrations"
+data Migration = Migration
+  { number :: String
+  , name :: String
+  } deriving (Show, Eq)
 
-  -- order files
-  pure files
+instance Ord Migration where
+  a `compare` b = (number a) `compare` (number b)
+
+toMigration string = Migration
+  { number = takeWhile (not . (== '_')) string
+  , name = string
+  }
+
+toMigrations :: [FilePath] -> [Migration]
+toMigrations = sort . fmap toMigration
+
+main = do
+  -- collect migrations
+  migrations <- toMigrations <$> filterDirs <$> listDirectory "./migrations"
+
+  -- parse migrations
+
+  -- lookup already run migrations
+
+  -- compare the two and run them
+
+  pure migrations
